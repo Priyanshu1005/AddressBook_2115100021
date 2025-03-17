f6df6ea08e7f66082e58a1ea5b77dc70ef717c9c
priyanshu <priyanshu.sharma_cs.ccv21@gla.ac.in>
2025-03-17 14:18:52 +0530

    [Priyanshu Sharma] ADD:error fixed

--- RepositoryLayer/Migrations/20250317063359_Initial.Designer.cs
+++ RepositoryLayer/Migrations/20250317063359_Initial.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace RepositoryLayer.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20250316093315_Initial")]
+    [Migration("20250317063359_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
